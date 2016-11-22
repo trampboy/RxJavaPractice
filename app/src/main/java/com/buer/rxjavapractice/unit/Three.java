c73451f05d673993c18f3c91ddd0ecd2ee7f1911
jinghonggang <honggangjing@gmail.com>
2016-11-22 23:58:49 +0800

    创建getLondonArtistCountTest

---
+++ app/src/main/java/com/buer/rxjavapractice/unit/Three.java
@@ -0,0 +1,8 @@
+package com.buer.rxjavapractice.unit;
+
+/**
+ * Created by YanMingDao on 22/11/2016.
+ */
+
+public class Three {
+}
---
+++ app/src/test/java/com/buer/rxjavapractice/unit/ThreeTest.java
@@ -0,0 +1,30 @@
+package com.buer.rxjavapractice.unit;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+ * Created by YanMingDao on 22/11/2016.
+ */
+
+public class ThreeTest {
+    Three three = new Three();
+    @Test
+    public void getLondonArtistCountTest() throws Exception {
+        ArrayList<Artist> arrArtist = new ArrayList<>();
+        Artist artist = new Artist();
+        artist.setFrom("China");
+        arrArtist.add(artist);
+        artist = new Artist();
+        artist.setFrom("London");
+        arrArtist.add(artist);
+        artist = new Artist();
+        artist.setFrom("China");
+        arrArtist.add(artist);
+        int count = three.getLondonArtistCount(arrArtist);
+        Assert.assertEquals(2, count);
+    }
+}
