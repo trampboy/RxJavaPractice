361b1535c56635cdc7a8a2f134e16711c3606004
jinghonggang <honggangjing@gmail.com>
2016-11-28 01:22:41 +0800

    Add EmptOrNeverOrErrorTest

---
+++ app/src/test/java/com/buer/rxjavapractice/creatingObservables/EmptOrNeverOrErrorTest.java
@@ -0,0 +1,74 @@
+package com.buer.rxjavapractice.creatingObservables;
+
+import org.junit.Test;
+
+import rx.Observable;
+import rx.Subscriber;
+
+/**
+ * Created by YanMingDao on 28/11/2016.
+ */
+
+public class EmptOrNeverOrErrorTest {
+    @Test
+    public void EmptyTest() throws Exception {
+        Observable.empty().subscribe(new Subscriber<Object>() {
+            @Override
+            public void onCompleted() {
+                System.out.println("onCompleted");
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                System.out.println("onError");
+            }
+
+            @Override
+            public void onNext(Object o) {
+                System.out.println("o:" + o);
+            }
+        });
+    }
+
+    @Test
+    public void NeverTest() throws Exception {
+        Observable.never().subscribe(new Subscriber<Object>() {
+            @Override
+            public void onCompleted() {
+                System.out.println("onCompleted");
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                System.out.println("onError");
+            }
+
+            @Override
+            public void onNext(Object o) {
+                System.out.println("o:" + o);
+            }
+        });
+    }
+
+    @Test
+    public void ErrorTest() throws Exception {
+        Observable.error(new NullPointerException("null"))
+                .subscribe(new Subscriber<Object>() {
+                    @Override
+                    public void onCompleted() {
+                        System.out.println("onCompleted");
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        System.out.println("onError");
+                    }
+
+                    @Override
+                    public void onNext(Object o) {
+                        System.out.println("o:" + o);
+                    }
+                });
+
+    }
+}
