92c907eb55c0da0e7cd2578926d8ab70d029e0a0
jinghonggang <honggangjing@gmail.com>
2016-12-04 17:38:02 +0800

    Add SkipActivity

--- app/src/main/java/com/buer/rxjavapractice/filteringObservables/SkipActivity.java
+++ app/src/main/java/com/buer/rxjavapractice/filteringObservables/SkipActivity.java
@@ -1,15 +1,53 @@
 package com.buer.rxjavapractice.filteringObservables;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 
+import com.buer.rxjavapractice.BaseActivity;
 import com.buer.rxjavapractice.R;
 
-public class SkipActivity extends AppCompatActivity {
+import java.util.concurrent.TimeUnit;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import rx.Observable;
+import rx.Subscriber;
+
+public class SkipActivity extends BaseActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_skip);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.btn_skip_test)
+    public void clickSkipTest() {
+        Observable.just(1,2,3).skip(2).subscribe(getSubscriber());
+    }
+
+    @OnClick(R.id.btn_skip_with_time_test)
+    public void clickSkipWithTimeTest() {
+        Observable.create(new Observable.OnSubscribe<Integer>() {
+            @Override
+            public void call(Subscriber<? super Integer> subscriber) {
+                if(subscriber.isUnsubscribed()) return;
+                try {
+                    //产生结果的间隔时间分别为100、200、300...900毫秒
+                    for (int i = 1; i < 10; i++) {
+                        subscriber.onNext(i);
+                        Thread.sleep(i * 100);
+                    }
+                    subscriber.onCompleted();
+                }catch(Exception e){
+                    subscriber.onError(e);
+                }
+            }
+        }).skip(500, TimeUnit.MILLISECONDS).subscribe(getSubscriber());
+    }
+
+    @Override
+    public int getContentLayoutId() {
+        return R.layout.activity_skip;
     }
 }
