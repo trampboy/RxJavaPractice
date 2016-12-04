ec6f74af88725f74dc9b9ae070e99afa880779a0
jinghonggang <honggangjing@gmail.com>
2016-12-04 17:29:47 +0800

    Add SampleActivity

--- app/src/main/java/com/buer/rxjavapractice/filteringObservables/SampleActivity.java
+++ app/src/main/java/com/buer/rxjavapractice/filteringObservables/SampleActivity.java
@@ -1,15 +1,72 @@
 package com.buer.rxjavapractice.filteringObservables;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 
+import com.buer.rxjavapractice.BaseActivity;
 import com.buer.rxjavapractice.R;
 
-public class SampleActivity extends AppCompatActivity {
+import java.util.concurrent.TimeUnit;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import rx.Observable;
+import rx.Subscriber;
+import rx.android.schedulers.AndroidSchedulers;
+
+public class SampleActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sample);
+        ButterKnife.bind(this);
+    }
+
+    @Override
+    public int getContentLayoutId() {
+        return R.layout.activity_sample;
+    }
+
+    @OnClick(R.id.btn_sample_test)
+    public void clickSampleTest() {
+        Observable.create(new Observable.OnSubscribe<Integer>() {
+            @Override
+            public void call(Subscriber<? super Integer> subscriber) {
+                if(subscriber.isUnsubscribed()) return;
+                try {
+                    //产生结果的间隔时间分别为100、200、300...900毫秒
+                    for (int i = 1; i < 10; i++) {
+                        subscriber.onNext(i);
+                        Thread.sleep(i * 100);
+                    }
+                    subscriber.onCompleted();
+                }catch(Exception e){
+                    subscriber.onError(e);
+                }
+            }
+        }).sample(500, TimeUnit.MILLISECONDS)
+                .subscribeOn(AndroidSchedulers.mainThread())
+                .subscribe(getSubscriber());
+    }
+
+    @OnClick(R.id.btn_throttle_first_test)
+    public void clickThrottleLastTest() {
+        Observable.create(new Observable.OnSubscribe<Integer>() {
+            @Override
+            public void call(Subscriber<? super Integer> subscriber) {
+                if(subscriber.isUnsubscribed()) return;
+                try {
+                    //产生结果的间隔时间分别为100、200、300...900毫秒
+                    for (int i = 1; i < 10; i++) {
+                        subscriber.onNext(i);
+                        Thread.sleep(i * 100);
+                    }
+                    subscriber.onCompleted();
+                }catch(Exception e){
+                    subscriber.onError(e);
+                }
+            }
+        }).throttleFirst(500, TimeUnit.MILLISECONDS)
+                .subscribeOn(AndroidSchedulers.mainThread())
+                .subscribe(getSubscriber());
     }
 }
